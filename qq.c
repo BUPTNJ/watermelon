c0c415affade718670c1e8cc4e6f41e7a2c21462
BUPTNJ <122296743@qq.com>
2020-10-04 17:55:10 +0800

    fix

--- qq.c
+++ qq.c
@@ -1,9 +1,13 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 dhfiuhsduigfsduhjfidsfidsjff
 =======
 dhfiuhsduigfsduhf
 >>>>>>> d2d9eb240a59e7be793a78f4776725876a752cf9
+=======
+dhfiuhsduigfsduhf
+>>>>>>> parent of 8cf026f... srp1
 =======
 dhfiuhsduigfsduhf
 >>>>>>> parent of 8cf026f... srp1
