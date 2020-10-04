ceac894d68844d2f3b9ff784140f1cebf1e7a003
BUPTNJ <122296743@qq.com>
2020-10-04 17:54:38 +0800

    fix

--- qq.c
+++ qq.c
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 dhfiuhsduigfsduhjfidsfidsjff
 =======
 dhfiuhsduigfsduhf
 >>>>>>> d2d9eb240a59e7be793a78f4776725876a752cf9
+=======
+dhfiuhsduigfsduhf
+>>>>>>> parent of 8cf026f... srp1
--- uu.cc
+++
@@ -1,2 +0,0 @@
-fndusifhisudhf
-fsdf
